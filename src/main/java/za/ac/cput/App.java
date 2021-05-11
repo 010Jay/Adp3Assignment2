e9d08470cba7aee1f4fa8452ef7c6309490d99c1
Jason Jaftha <jsnjaftha3@gmail.com>
2021-05-11 18:33:29 +0200

    Add Dog Class and Remove the App and AppTest class.

--- src/main/java/za/ac/cput/App.java
+++
@@ -1,6 +0,0 @@
-package za.ac.cput;
-
-public class App {
-
-
-}
---
+++ src/main/java/za/ac/cput/Dog.java
@@ -0,0 +1,13 @@
+package za.ac.cput;
+
+public class Dog {
+
+    //Attributes
+        private String name;
+
+    public String talk()
+    {
+        return "Woof!";
+    }
+
+}
