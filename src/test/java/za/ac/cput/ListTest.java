295c4a026059b45ab2ea32750190df30b7e45c3e
Jason Jaftha <jsnjaftha3@gmail.com>
2021-05-13 16:59:53 +0200

    Finish ListTest class, Project completed.

--- src/test/java/za/ac/cput/ListTest.java
+++ src/test/java/za/ac/cput/ListTest.java
@@ -13,8 +13,56 @@ import java.util.*;
 
 public class ListTest {
 
+    List<String> employeeList = new ArrayList<>();
+
     @BeforeEach
     void setUp(){
+
+        //Add method | Add 3 employees to the employeeList
+            employeeList.add("Bob");
+            employeeList.add("Elena");
+            employeeList.add("Sofia");
+    }
+
+    @Test
+    public void testAdd()
+    {
+        int size = employeeList.size();
+
+        System.out.println(employeeList);
+
+        assertEquals(3, size); //Expects 3 elements
+    }
+
+    @Test
+    public void testRemove()
+    {
+        //Remove method | remove element 1 "Bob" from the employeeList
+            employeeList.remove("Bob");
+
+        int size = employeeList.size();
+
+        System.out.println(employeeList);
+
+        assertEquals(2, size); //Expects 2 elements
+    }
+
+    @Test
+    public void testFind()
+    {
+        String source = "Elena"; //Element(Elena) to be check if it does exist in the employeeList.
+        boolean found = employeeList.contains(source);
+
+        if(found)
+        {
+            System.out.println("The employee " + source + " does exist in the employeeList.");
+        }
+        else
+        {
+            System.out.println("The employee " + source + " does not exist in the employeeList.");
+        }
+
+        assertTrue(found); //Expects true;
 
     }
 }
